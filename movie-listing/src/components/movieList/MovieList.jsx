3c097404c0363cf4f7d7a10cc24bdc6033b2e82f
nikhilsomani <nikhilsomani15@gmail.com>
2025-12-01 23:58:08 +0530

    movie-listing application work updated

--- movie-listing/src/components/movieList/MovieList.jsx
+++ movie-listing/src/components/movieList/MovieList.jsx
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import MovieCard from "./MovieCard";
 
 const MovieList = () => {
-    const [movies, setMovies] = useState([]);
+  const [movieList, setMovieList] = useState([]);
 
-    useEffect(() => {
-        fetchMovies();
-    }, []);
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/authentication/token/new",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
+    },
+  };
 
-    const fetchMovies = async () => {
-        const response = await fetch(
-            "https://api.themoviedb.org/3/movie/popular?api_key=183928bab7fc630ed0449e4f66ec21bd"
-        );
-        const data = await response.json();
-        setMovies(data.results);
-    };
+  useEffect(() => {
+    // using axios
+    // const data=axios
+    //   .request(options)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => console.error(err));
 
-    return (
-        <section className='movie_list'>
-            <header className='align_center movie_list_header'>
-                <h2 className='align_center movie_list_heading'>
-                    Popular{" "}
-                    <img src={Fire} alt='fire emoji' className='navbar_emoji' />
-                </h2>
+    fetchMovieData();
+  }, []);
 
-                <div className='align_center movie_list_fs'>
-                    <ul className='align_center movie_filter'>
-                        <li className='movie_filter_item active'>8+ Star</li>
-                        <li className='movie_filter_item'>7+ Star</li>
-                        <li className='movie_filter_item'>6+ Star</li>
-                    </ul>
-
-                    <select name='' id='' className='movie_sorting'>
-                        <option value=''>SortBy</option>
-                        <option value=''>Date</option>
-                        <option value=''>Rating</option>
-                    </select>
-                    <select name='' id='' className='movie_sorting'>
-                        <option value=''>Ascending</option>
-                        <option value=''>Descending</option>
-                    </select>
-                </div>
-            </header>
-
-            <div className='movie_cards'>
-                {movies.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-            </div>
-        </section>
+  const fetchMovieData = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/popular",
+      options
     );
+    const data = await res.json();
+    setMovieList(data.results);
+    console.log(data);
+  };
+
+  return (
+    <section className="movie_list">
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+        </h2>
+
+        <div className="align_center movie_list_fs">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
+
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
+        </div>
+      </header>
+
+      <div className="movie_cards">
+        {movieList.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default MovieList;
