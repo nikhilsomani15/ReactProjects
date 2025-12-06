d62b79234ef3c371920a7f9b3b032ced1b7ed7cc
nikhilsomani <nikhilsomani15@gmail.com>
2025-12-07 00:29:49 +0530

    e commerce app cart and product  updated

--- e-commerce-app/src/App.jsx
+++ e-commerce-app/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import HomePage from "./components/home/HomePage";
+import Navbar from "./components/navbar/Navbar";
+import SingleProductPage from "./components/singleproducts/SingleProductPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +13,8 @@ function App() {
     <div className="app">
       <Navbar />
       <main>
-        <HomePage />
+        {/* <HomePage /> */}
+        <SingleProductPage />
       </main>
     </div>
   );
--- e-commerce-app/src/components/Products/ProductsSidebar.jsx
+++ e-commerce-app/src/components/Products/ProductsSidebar.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
-import LinkWithIcon from "../Navbar/LinkWithIcon";
+import LinkWithIcon from "../navbar/LinkWithIcon";
 
 const ProductsSidebar = () => {
-    return (
-        <aside className='products_sidebar'>
-            <h2>Category</h2>
+  return (
+    <aside className="products_sidebar">
+      <h2>Category</h2>
 
-            <div className='category_links'>
-                <LinkWithIcon
-                    title='Electronics'
-                    link='products?category=electronics'
-                    emoji={rocket}
-                    sidebar={true}
-                />
-            </div>
-        </aside>
-    );
+      <div className="category_links">
+        <LinkWithIcon
+          title="Electronics"
+          link="products?category=electronics"
+          emoji={rocket}
+          sidebar={true}
+        />
+      </div>
+    </aside>
+  );
 };
 
 export default ProductsSidebar;
